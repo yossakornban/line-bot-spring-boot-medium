74fe53bbf48b53b9bd5e4674a4d236d815faf812
Ittiwat_n <ittiwat.nir@gmail.com>
2019-03-07 11:16:59 +0700

    no message

--- src/main/java/com/iphayao/linebot/LineBotController.java
+++ src/main/java/com/iphayao/linebot/LineBotController.java
@@ -1,26 +1,5 @@
 package com.iphayao.linebot;
 
-import com.google.common.io.ByteStreams;
-import com.linecorp.bot.client.LineMessagingClient;
-import com.linecorp.bot.client.MessageContentResponse;
-import com.linecorp.bot.model.ReplyMessage;
-import com.linecorp.bot.model.event.Event;
-import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.ImageMessageContent;
-import com.linecorp.bot.model.event.message.LocationMessageContent;
-import com.linecorp.bot.model.event.message.StickerMessageContent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.*;
-import com.linecorp.bot.model.response.BotApiResponse;
-import com.linecorp.bot.spring.boot.annotation.EventMapping;
-import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
-
-import lombok.NonNull;
-import lombok.Value;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.UncheckedIOException;
@@ -30,184 +9,227 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
-import org.springframework.jdbc.core.JdbcTemplate;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.google.common.io.ByteStreams;
+import com.iphayao.linebot.flex.CatalogueFlexMessageSupplier;
+import com.iphayao.linebot.flex.NewsFlexMessageSupplier;
+import com.iphayao.linebot.flex.ReceiptFlexMessageSupplier;
+import com.iphayao.linebot.flex.RestaurantFlexMessageSupplier;
+import com.iphayao.linebot.flex.RestaurantMenuFlexMessageSupplier;
+import com.iphayao.linebot.flex.TicketFlexMessageSupplier;
+import com.iphayao.linebot.helper.RichMenuHelper;
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.client.MessageContentResponse;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.ImageMessageContent;
+import com.linecorp.bot.model.event.message.LocationMessageContent;
+import com.linecorp.bot.model.event.message.StickerMessageContent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.ImageMessage;
+import com.linecorp.bot.model.message.LocationMessage;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.StickerMessage;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+import lombok.NonNull;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @ComponentScan
 @LineMessageHandler
 public class LineBotController {
-    @Autowired
-    private LineMessagingClient lineMessagingClient;
-    
-    @Autowired
-    private JdbcTemplate jdbc;
+	@Autowired
+	private LineMessagingClient lineMessagingClient;
 
-    @EventMapping
-    public void handleTextMessage(MessageEvent<TextMessageContent> event) {
-        log.info(event.toString());
-        TextMessageContent message = event.getMessage();
-        handleTextContent(event.getReplyToken(), event, message);
-    }
+	@EventMapping
+	public void handleTextMessage(MessageEvent<TextMessageContent> event) throws IOException {
+		log.info(event.toString());
+		TextMessageContent message = event.getMessage();
+		handleTextContent(event.getReplyToken(), event, message);
+	}
 
-    @EventMapping
-    public void handleStickerMessage(MessageEvent<StickerMessageContent> event) {
-        log.info(event.toString());
-        StickerMessageContent message = event.getMessage();
-        reply(event.getReplyToken(), new StickerMessage(
-                message.getPackageId(), message.getStickerId()
-        ));
-    }
+	@EventMapping
+	public void handleStickerMessage(MessageEvent<StickerMessageContent> event) {
+		log.info(event.toString());
+		StickerMessageContent message = event.getMessage();
+		reply(event.getReplyToken(), new StickerMessage(message.getPackageId(), message.getStickerId()));
+	}
 
-    @EventMapping
-    public void handleLocationMessage(MessageEvent<LocationMessageContent> event) {
-        log.info(event.toString());
-        LocationMessageContent message = event.getMessage();
-        reply(event.getReplyToken(), new LocationMessage(
-                (message.getTitle() == null) ? "Location replied" : message.getTitle(),
-                message.getAddress(),
-                message.getLatitude(),
-                message.getLongitude()
-        ));
-    }
+	@EventMapping
+	public void handleLocationMessage(MessageEvent<LocationMessageContent> event) {
+		log.info(event.toString());
+		LocationMessageContent message = event.getMessage();
+		reply(event.getReplyToken(),
+				new LocationMessage((message.getTitle() == null) ? "Location replied" : message.getTitle(),
+						message.getAddress(), message.getLatitude(), message.getLongitude()));
+	}
 
-    @EventMapping
-    public void handleImageMessage(MessageEvent<ImageMessageContent> event) {
-        log.info(event.toString());
-        ImageMessageContent content = event.getMessage();
-        String replyToken = event.getReplyToken();
+	@EventMapping
+	public void handleImageMessage(MessageEvent<ImageMessageContent> event) {
+		log.info(event.toString());
+		ImageMessageContent content = event.getMessage();
+		String replyToken = event.getReplyToken();
 
-        try {
-            MessageContentResponse response = lineMessagingClient.getMessageContent(content.getId()).get();
-            DownloadedContent jpg = saveContent("jpg", response);
-            DownloadedContent previewImage = createTempFile("jpg");
+		try {
+			MessageContentResponse response = lineMessagingClient.getMessageContent(content.getId()).get();
+			DownloadedContent jpg = saveContent("jpg", response);
+			DownloadedContent previewImage = createTempFile("jpg");
 
-            system("convert", "-resize", "240x",
-                    jpg.path.toString(),
-                    previewImage.path.toString());
+			system("convert", "-resize", "240x", jpg.path.toString(), previewImage.path.toString());
 
-            reply(replyToken, new ImageMessage(jpg.getUri(), previewImage.getUri()));
+			reply(replyToken, new ImageMessage(jpg.getUri(), previewImage.getUri()));
 
-        } catch (InterruptedException | ExecutionException e) {
-            reply(replyToken, new TextMessage("Cannot get image: " + content));
-            throw new RuntimeException(e);
-        }
+		} catch (InterruptedException | ExecutionException e) {
+			reply(replyToken, new TextMessage("Cannot get image: " + content));
+			throw new RuntimeException(e);
+		}
 
-    }
+	}
 
-    private void handleTextContent(String replyToken, Event event, TextMessageContent content) {
-        String text = content.getText();
+	private void handleTextContent(String replyToken, Event event, TextMessageContent content) throws IOException {
+		String text = content.getText();
+		String userId = event.getSource().getUserId();
 
-        log.info("Got text message from %s : %s", replyToken, text);
+		log.info("Got text message from %s : %s", replyToken, text);
 
-        switch (text) {
-            case "Profile": {
-                String userId = event.getSource().getUserId();
-                if(userId != null) {
-                    lineMessagingClient.getProfile(userId)
-                            .whenComplete((profile, throwable) -> {
-                                if(throwable != null) {
-                                    this.replyText(replyToken, throwable.getMessage());
-                                    return;
-                                }
-                                this.reply(replyToken, Arrays.asList(
-                                        new TextMessage("Display name: " + profile.getDisplayName()),
-                                        new TextMessage("Status message: " + profile.getStatusMessage()),
-                                        new TextMessage("User ID: " + profile.getUserId())
-                                ));
-                            });
-                }
-                break;
-            }
-            default:
-                List<Map<String, Object>> list2 = jdbc.queryForList
-    			("SELECT username FROM admin_user");
-                
-                for (Map<String, Object> map : list2) 
-                { 
-                    log.info("Return echo message key username : " + map.get("username"));
-                }
-                log.info("Return echo message %s : %s", replyToken, text);
-                this.replyText(replyToken, text);
-        }
-    }
+		switch (text) {
+		case "Flex": {
+			String pathImageFlex = new ClassPathResource("asset/richmenu-home.jpg").getFile().getAbsolutePath();
+			String pathConfigFlex = new ClassPathResource("asset/richmenu-home.yml").getFile().getAbsolutePath();
+			RichMenuHelper.createRichMenu(lineMessagingClient, pathConfigFlex, pathImageFlex, userId);
+			break;
+		}
+		case "Flex Back": {
 
-    private void handleStickerContent(String replyToken, StickerMessageContent content) {
-        reply(replyToken, new StickerMessage(
-                content.getPackageId(), content.getStickerId()
-        ));
-    }
+			RichMenuHelper.deleteRichMenu(lineMessagingClient, userId);
+			break;
+		}
 
-    private void replyText(@NonNull  String replyToken, @NonNull String message) {
-        if(replyToken.isEmpty()) {
-            throw new IllegalArgumentException("replyToken is not empty");
-        }
+		case "Flex Restaurant": {
+			this.reply(replyToken, new RestaurantFlexMessageSupplier().get());
+			break;
+		}
+		case "Flex Menu": {
+			this.reply(replyToken, new RestaurantMenuFlexMessageSupplier().get());
+			break;
+		}
+		case "Flex Receipt": {
+			this.reply(replyToken, new ReceiptFlexMessageSupplier().get());
+			break;
+		}
+		case "Flex News": {
+			this.reply(replyToken, new NewsFlexMessageSupplier().get());
+			break;
+		}
+		case "Flex Ticket": {
+			this.reply(replyToken, new TicketFlexMessageSupplier().get());
+			break;
+		}
+		case "Flex Catalogue": {
+			this.reply(replyToken, new CatalogueFlexMessageSupplier().get());
+			break;
+		}
+		case "Profile": {
+			if (userId != null) {
+				lineMessagingClient.getProfile(userId).whenComplete((profile, throwable) -> {
+					if (throwable != null) {
+						this.replyText(replyToken, throwable.getMessage());
+						return;
+					}
+					this.reply(replyToken,
+							Arrays.asList(new TextMessage("Display name: " + profile.getDisplayName()),
+									new TextMessage("Status message: " + profile.getStatusMessage()),
+									new TextMessage("User ID: " + profile.getUserId())));
+				});
+			}
+			break;
+		}
+		default:
+			log.info("Return echo message %s : %s", replyToken, text);
+			this.replyText(replyToken, text);
+		}
+	}
 
-        if(message.length() > 1000) {
-            message = message.substring(0, 1000 - 2) + "...";
-        }
-        this.reply(replyToken, new TextMessage(message));
-    }
+//	private void handleStickerContent(String replyToken, StickerMessageContent content) {
+//		reply(replyToken, new StickerMessage(content.getPackageId(), content.getStickerId()));
+//	}
 
-    private void reply(@NonNull String replyToken, @NonNull Message message) {
-        reply(replyToken, Collections.singletonList(message));
-    }
+	private void replyText(@NonNull String replyToken, @NonNull String message) {
+		if (replyToken.isEmpty()) {
+			throw new IllegalArgumentException("replyToken is not empty");
+		}
 
-    private void reply(@NonNull String replyToken, @NonNull List<Message> messages) {
-        try {
-            BotApiResponse response = lineMessagingClient.replyMessage(
-                    new ReplyMessage(replyToken, messages)
-            ).get();
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-    }
+		if (message.length() > 1000) {
+			message = message.substring(0, 1000 - 2) + "...";
+		}
+		this.reply(replyToken, new TextMessage(message));
+	}
 
-    private void system(String... args) {
-        ProcessBuilder processBuilder = new ProcessBuilder(args);
-        try {
-            Process start = processBuilder.start();
-            int i = start.waitFor();
-            log.info("result: {} => {}", Arrays.toString(args), i);
-        } catch (InterruptedException e) {
-            log.info("Interrupted", e);
-            Thread.currentThread().interrupt();
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
-    }
+	private void reply(@NonNull String replyToken, @NonNull Message message) {
+		reply(replyToken, Collections.singletonList(message));
+	}
 
-    private static DownloadedContent saveContent(String ext, MessageContentResponse response) {
-        log.info("Content-type: {}", response);
-        DownloadedContent tempFile = createTempFile(ext);
-        try (OutputStream outputStream = Files.newOutputStream(tempFile.path)) {
-            ByteStreams.copy(response.getStream(), outputStream);
-            log.info("Save {}: {}", ext, tempFile);
-            return tempFile;
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
-    }
+	private void reply(@NonNull String replyToken, @NonNull List<Message> messages) {
+		try {
+			lineMessagingClient.replyMessage(new ReplyMessage(replyToken, messages)).get();
+		} catch (InterruptedException | ExecutionException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    private static DownloadedContent createTempFile(String ext) {
-        String fileName = LocalDateTime.now() + "-" + UUID.randomUUID().toString() + "." + ext;
-        Path tempFile = Application.downloadedContentDir.resolve(fileName);
-        tempFile.toFile().deleteOnExit();
-        return new DownloadedContent(tempFile, createUri("/downloaded/" + tempFile.getFileName()));
+	private void system(String... args) {
+		ProcessBuilder processBuilder = new ProcessBuilder(args);
+		try {
+			Process start = processBuilder.start();
+			int i = start.waitFor();
+			log.info("result: {} => {}", Arrays.toString(args), i);
+		} catch (InterruptedException e) {
+			log.info("Interrupted", e);
+			Thread.currentThread().interrupt();
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
 
-    }
+	private static DownloadedContent saveContent(String ext, MessageContentResponse response) {
+		log.info("Content-type: {}", response);
+		DownloadedContent tempFile = createTempFile(ext);
+		try (OutputStream outputStream = Files.newOutputStream(tempFile.path)) {
+			ByteStreams.copy(response.getStream(), outputStream);
+			log.info("Save {}: {}", ext, tempFile);
+			return tempFile;
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
 
-    private static String createUri(String path) {
-        return ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path(path).toUriString();
-    }
+	private static DownloadedContent createTempFile(String ext) {
+		String fileName = LocalDateTime.now() + "-" + UUID.randomUUID().toString() + "." + ext;
+		Path tempFile = Application.downloadedContentDir.resolve(fileName);
+		tempFile.toFile().deleteOnExit();
+		return new DownloadedContent(tempFile, createUri("/downloaded/" + tempFile.getFileName()));
 
-    @Value
-    public static class DownloadedContent {
-        Path path;
-        String uri;
-    }
+	}
+
+	private static String createUri(String path) {
+		return ServletUriComponentsBuilder.fromCurrentContextPath().path(path).toUriString();
+	}
+
+	@Value
+	public static class DownloadedContent {
+		Path path;
+		String uri;
+	}
 }
