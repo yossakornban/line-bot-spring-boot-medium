5084b8c43538a8ecc718cdfe003f3a41a8c91543
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-03-15 09:21:40 +0700

    no message

--- src/main/java/com/iphayao/linebot/model/Holiday.java
+++ src/main/java/com/iphayao/linebot/model/Holiday.java
@@ -7,17 +7,23 @@ import lombok.Data;
 
 public class Holiday {
 
-	private Date date_holiday;
+	private String date_holiday;
 	private String name_holiday;
-
+	
+	
 	public String getDate_holiday() {
-		// TODO Auto-generated method stub
-		return null;
+		return date_holiday;
+	}
+	public void setDate_holiday(String date_holiday) {
+		this.date_holiday = date_holiday;
+	}
+	public String getName_holiday() {
+		return name_holiday;
+	}
+	public void setName_holiday(String name_holiday) {
+		this.name_holiday = name_holiday;
 	}
 
-	public String getNname_holiday() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 
 }
