1d8339ca6a9bc1937f0defa943bca299548cf0cb
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-03-15 10:58:55 +0700

    no message

--- src/main/java/com/iphayao/linebot/LineBotController.java
+++ src/main/java/com/iphayao/linebot/LineBotController.java
@@ -166,8 +166,8 @@ public class LineBotController {
 					modelMapper.map(record, holi);
 					holi_list.push(holi.getDate_holiday()+"  "+holi.getName_holiday());
 					String Imr = holi_list.toString();
-					System.out.println(Imr);
-					this.reply(replyToken, Arrays.asList(new TextMessage(Imr)));
+					System.out.println("String is :"+Imr);
+					System.out.print(holi_list.pop());
 					
 					
 				});
