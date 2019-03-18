907efd4f9a61f928b066d17b1bf6cafdf5d62bb8
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-03-18 14:03:58 +0700

    no message

--- src/main/java/com/iphayao/linebot/LineRepository.java
+++ src/main/java/com/iphayao/linebot/LineRepository.java
@@ -116,7 +116,7 @@ public class LineRepository {
 			jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
 			stb = new StringBuilder();
 
-			stb.append("SELECT name_holiday, to_date(date_holiday, 'dd/mm/yyyy') FROM holiday WHERE to_date(date_holiday, 'dd/mm/yyyy') BETWEEN now() and to_date('31/12/2019', 'dd/mm/yyyy') ORDER BY to_date limit 3");
+			stb.append("select to_date(date_holiday, 'dd/mm/yyyy'),name_holiday from holiday where to_date(date_holiday, 'dd/mm/yyyy') between now() and to_date('31/12/2019', 'dd/mm/yyyy') order by to_date  limit 5");
 
 			MapSqlParameterSource parameters = new MapSqlParameterSource();
 
