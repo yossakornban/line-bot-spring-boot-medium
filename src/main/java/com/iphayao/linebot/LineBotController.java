26b017889fd705aef969dcfe8007b0e7c5865eaa
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-03-19 11:01:33 +0700

    no message

--- src/main/java/com/iphayao/linebot/LineBotController.java
+++ src/main/java/com/iphayao/linebot/LineBotController.java
@@ -197,6 +197,7 @@ public class LineBotController {
 
 				userLog.setStatusBot(status.DEFAULT);
 				break;
+				
 			}
 			
 			// ----------------------------------------------------------------------------------------------------------------Find
