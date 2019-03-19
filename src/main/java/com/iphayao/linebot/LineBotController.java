2861c5cd48aa280a94e9c9128a8658cb449c65c1
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-03-19 12:01:47 +0700

    no message

--- src/main/java/com/iphayao/linebot/LineBotController.java
+++ src/main/java/com/iphayao/linebot/LineBotController.java
@@ -200,6 +200,34 @@ public class LineBotController {
 				
 			}
 			
+			
+			case "ขอทราบวันหยุด ทั้งหมดภายในปีนี้ ค่ะ": {
+				String pathYamlHome = "asset/sub_select_event.yml";
+				String pathImageHome = "asset/sub_select_event.jpg";
+				RichMenuHelper.createRichMenu(lineMessagingClient, pathYamlHome, pathImageHome, userLog.getUserID());
+				this.reply(replyToken, Arrays.asList(new TextMessage("เลือกเมนูที่ต้องการ ได้เลยค่ะ ^^"
+						)));
+				Stack<String> holi_list = new Stack<>();
+				ArrayList<Map<String, Object>> holiday_all = lineRepo.holidayList();
+				holiday_all.forEach(record -> {
+					Holiday holi = new Holiday();
+					modelMapper.map(record, holi);
+
+					holi_list.push("\n" + holi.getDate_holiday() + "  " + holi.getName_holiday());
+
+				});
+				String Imr = holi_list.toString();
+				Imr = Imr.replace("[", "");
+				Imr = Imr.replace("]", "");
+				Imr = Imr.replace(",", "");
+				this.reply(replyToken,
+						Arrays.asList(new TextMessage("ข้อมูลวันหยุดประจำปี ได้เเล้วค่ะ ^^" + "\n" + Imr)));
+
+				userLog.setStatusBot(status.DEFAULT);
+				break;
+				
+			}
+			
 			// ----------------------------------------------------------------------------------------------------------------Find
 			// Three day holiday
 			case "ขอทราบวันหยุดที่จะมาถึงเร็วๆนี้ ค่ะ": {
