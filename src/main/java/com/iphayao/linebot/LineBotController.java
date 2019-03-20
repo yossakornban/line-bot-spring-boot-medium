b4f55a4a8f16925ac5f6464272511cf62751a0ce
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-03-20 11:04:35 +0700

    no message

--- src/main/java/com/iphayao/linebot/LineBotController.java
+++ src/main/java/com/iphayao/linebot/LineBotController.java
@@ -241,7 +241,7 @@ public class LineBotController {
 				holiday_all.forEach(record -> {
 					Holiday holi = new Holiday();
 					modelMapper.map(record, holi);
-					holi_list.push("\n" + holi.getDate_holiday() + " :  " + holi.getName_holiday());
+					holi_list.push("\n" + holi.getDate_holiday() + "   " + holi.getName_holiday());
 
 				});
 				String day1 = holiday_all.get(0).toString();
