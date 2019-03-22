7880e1cf01430b5fc0a341afdb945c4f8be3ac66
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-03-22 16:39:20 +0700

    no message

--- src/main/java/com/iphayao/linebot/LineBotController.java
+++ src/main/java/com/iphayao/linebot/LineBotController.java
@@ -449,7 +449,8 @@ public class LineBotController {
 				break;
 			}
 			case "Vote": {
-				String imageUrl = createUri("asset/kawpad.jpg");
+				
+				String imageUrl = createUri("asset/richmenu-home.jpg");
 				CarouselTemplate carouselTemplate = new CarouselTemplate(Arrays.asList(
 					
 						new CarouselColumn(imageUrl, "ข้าวผัด", "เพิ่มพลังความคิด วิตามีน B12 ให้โปรเเกรมเมอร์ ทีม Java",
