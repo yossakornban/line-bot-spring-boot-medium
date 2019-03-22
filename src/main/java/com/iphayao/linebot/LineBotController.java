588903d2406025f4ec8eb9ef30243bb77131cf1a
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-03-22 12:10:36 +0700

    no message

--- src/main/java/com/iphayao/linebot/LineBotController.java
+++ src/main/java/com/iphayao/linebot/LineBotController.java
@@ -539,7 +539,7 @@ public class LineBotController {
 		} else if (userLog.getStatusBot().equals(status.FINDEMP)) {
 
 			if (empName != null) {
-				
+				lineRepo.register(userLog);
 				ConfirmTemplate confirmTemplate = new ConfirmTemplate("ยืนยัน, คุณใช่ " + empName + " หรือไม่ ?",
 						new MessageAction("ใช่ !", "ใช่"), new MessageAction("ไม่ใช่ !", "ไม่ใช่"));
 
@@ -562,7 +562,7 @@ public class LineBotController {
 
 				
 				
-				lineRepo.register(userLog);
+			
 				userLog.setStatusBot(status.DEFAULT);
 				String pathYamlHome = "asset/select_event.yml";
 				String pathImageHome = "asset/select_event.jpg";
