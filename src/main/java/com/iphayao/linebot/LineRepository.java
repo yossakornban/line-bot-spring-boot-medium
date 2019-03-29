d73519d8aed5efcf58c80d38021832ef9a449846
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-03-29 16:52:00 +0700

    no message

--- src/main/java/com/iphayao/linebot/LineRepository.java
+++ src/main/java/com/iphayao/linebot/LineRepository.java
@@ -82,8 +82,8 @@ public class LineRepository {
 			Date date = new Date();
 			System.out.println(dateFormat.format(date)); //2016/11/16 12:08:43
 			
-			//stb.append(" insert into testadd(Emp_id) values(employeeid)");
-			stb.append(" UPDATE testadd SET emp_id = :foodIdVote");
+			stb.append("insert into testadd  values(employeeid,foodIdVote,dateNow)");
+			
 	   	    // stb.append("insert into testadd (emp_id,food_id,date)  values  (employeeid,foodIdVote,dateNow)");
 			//stb.append(" WHERE emp_emp_code = :empcode ");
 		    
