fc06a9e7e4657ba2ceefb86c3f35b14c0fad1c44
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-04-01 17:23:22 +0700

    no message

--- src/main/java/com/iphayao/linebot/LineRepository.java
+++ src/main/java/com/iphayao/linebot/LineRepository.java
@@ -49,24 +49,24 @@ public class LineRepository {
 	private NamedParameterJdbcTemplate jdbcTemplate = null;
 	private StringBuilder stb = null;
 	
-	public String CountVote (UserLog  userLog) {
-		ArrayList<Map<String, Object>> result = null;
-		// List<Map<String, Object>> result = null;
-		try {
-			jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
-			stb = new StringBuilder();
-			stb.append(" select count(emp_emp_id) from employee_vote where emp_emp_id =:empcode ");
-			MapSqlParameterSource parameters = new MapSqlParameterSource();
-			parameters.addValue("empcode", userLog.getEmpCode());
-			result = (ArrayList<Map<String, Object>>) jdbcTemplate.queryForList(stb.toString(), parameters);
-			if (result.size() == 0) {
-				return null;
-			}
-		} catch (EmptyResultDataAccessException ex) {
-			log.error("Msg :: {}, Trace :: {}", ex.getMessage(), ex.getStackTrace());
-		}
-		return (String) result.get(0).get("count");
-	}
+//	public String CountVote (UserLog  userLog) {
+//		ArrayList<Map<String, Object>> result = null;
+//		// List<Map<String, Object>> result = null;
+//		try {
+//			jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+//			stb = new StringBuilder();
+//			stb.append(" select count(emp_emp_id) from employee_vote where emp_emp_id =:empcode ");
+//			MapSqlParameterSource parameters = new MapSqlParameterSource();
+//			parameters.addValue("empcode", userLog.getEmpCode());
+//			result = (ArrayList<Map<String, Object>>) jdbcTemplate.queryForList(stb.toString(), parameters);
+//			if (result.size() == 0) {
+//				return null;
+//			}
+//		} catch (EmptyResultDataAccessException ex) {
+//			log.error("Msg :: {}, Trace :: {}", ex.getMessage(), ex.getStackTrace());
+//		}
+//		return (String) result.get(0).get("count");
+//	}
 	public int register(UserLog userLog) {
 		int aaa = 0;
 		try {
@@ -185,7 +185,6 @@ public class LineRepository {
 		}
 		return result;
 	}
-
 	public ArrayList<Map<String, Object>> list() {
 		ArrayList<Map<String, Object>> result = null;
 		// List<Map<String, Object>> result = null;
@@ -200,5 +199,4 @@ public class LineRepository {
 		}
 		return result;
 	}
-
 }
