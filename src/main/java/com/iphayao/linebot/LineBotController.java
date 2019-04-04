d080471c36e0ef675e9d87e540d0728bf89d8deb
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-04-04 13:51:41 +0700

    no message

--- src/main/java/com/iphayao/linebot/LineBotController.java
+++ src/main/java/com/iphayao/linebot/LineBotController.java
@@ -173,9 +173,8 @@ public class LineBotController {
 			}
 			case "ขอทราบ ข้อมูลวันหยุดค่ะ": {
 											//--------------------------------Focus
-				System.out.println(userLog);
-				HolidayController hhh = new HolidayController();
 				userLog.setTextInputFromUser(text);
+				HolidayController hhh = new HolidayController();
 				hhh.handleTextContent(foodName, event, content);
 				System.out.println(text);
 				
