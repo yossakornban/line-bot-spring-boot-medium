0f4593700b12e9f6ab01bd9bb1fc463eac10a748
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-04-04 13:45:26 +0700

    no message

--- src/main/java/sub_controller/HolidayController.java
+++ src/main/java/sub_controller/HolidayController.java
@@ -141,7 +141,7 @@ public class HolidayController {
 	Date nowDate = new Date();
 
 	public void handleTextContent(String replyToken, Event event, TextMessageContent content) throws IOException {
-		UserLog userLog = userMap.get(event.getSource().getSenderId());
+		UserLog userLog = new UserLog();
 		
 		if (userLog == null) {
 			userLog = new UserLog(event.getSource().getSenderId(), status.DEFAULT);
@@ -150,11 +150,11 @@ public class HolidayController {
 		ModelMapper modelMapper = new ModelMapper();
 		
 		System.out.println(userLog.getTextInputFromUser()+"444444444444444444444444444444444444");
-		
+	
 
 		if (userLog.getStatusBot().equals(status.DEFAULT)) {
 			switch (userLog.getTextInputFromUser()) {
-			case "ขอทราบ ข้อมูลวันหยุดค่ะ": {
+			case "111": {
 				System.out.println("Raider Striker :33333333333333333333333333333333");
 				String pathYamlHome = "asset/sub_select_event.yml";
 				String pathImageHome = "asset/sub_select_event.jpg";
