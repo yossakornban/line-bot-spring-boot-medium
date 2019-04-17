2cea9e39ad0931476d343297d3a025dddbb5f800
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-04-17 15:52:00 +0700

    no message

--- src/main/java/com/iphayao/repository/LineBot_Repo.java
+++ src/main/java/com/iphayao/repository/LineBot_Repo.java
@@ -18,7 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-//import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.iphayao.linebot.model.UserLog;
