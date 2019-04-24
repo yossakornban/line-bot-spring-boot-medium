d409231547e77ee226bea9ac027ae4a6deec2be9
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-04-24 10:29:33 +0700

    no message

--- src/main/java/com/iphayao/linebot/Application.java
+++ src/main/java/com/iphayao/linebot/Application.java
@@ -1,11 +1,7 @@
 package com.iphayao.linebot;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.iphayao.LineApplication;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,10 +9,10 @@ import java.nio.file.Path;
 
 @SpringBootApplication
 public class Application {
-  public static Path downloadedContentDir;
-	public static void main(String[] args) throws IOException {
-	downloadedContentDir = Files.createTempDirectory("line-bot");
-	SpringApplication.run(LineApplication.class, args);
-	}
+    static Path downloadedContentDir;
 
+    public static void main(String[] args) throws IOException {
+        downloadedContentDir = Files.createTempDirectory("line-bot");
+        SpringApplication.run(Application.class, args);
+    }
 }
