fd2670ccfdda92810b6e624c5abb159e0fdae801
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-04-25 10:45:09 +0700

    no message

--- src/main/java/com/iphayao/LineApplication.java
+++ src/main/java/com/iphayao/LineApplication.java
@@ -3,9 +3,6 @@ package com.iphayao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.iphayao.linebot.foodsController;
-import com.iphayao.linebot.holidayController;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,11 +15,6 @@ public class LineApplication {
 	public static void main(String[] args) throws IOException {
 	downloadedContentDir = Files.createTempDirectory("line-bot");
 	SpringApplication.run(LineApplication.class, args);
-	holidayController holiday = new holidayController();
-	holiday.getClass();
-	foodsController foods = new foodsController();
-	foods.getClass();
-
-	
 	}
+
 }
