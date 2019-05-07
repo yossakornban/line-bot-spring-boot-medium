1bceeed0f90609cd6a46921a455e4cd4e1f30121
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-05-07 13:10:55 +0700

    no message

--- src/main/java/com/iphayao/linebot/DashboardFoodVoteController.java
+++ src/main/java/com/iphayao/linebot/DashboardFoodVoteController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.iphayao.repository.Holiday_Repo;
 
 import lombok.extern.slf4j.Slf4j;
 
