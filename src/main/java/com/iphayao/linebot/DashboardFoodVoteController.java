6bc793a23409ab2fbf91ef569e9f281a85652dce
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-05-07 13:18:03 +0700

    no message

--- src/main/java/com/iphayao/linebot/DashboardFoodVoteController.java
+++ src/main/java/com/iphayao/linebot/DashboardFoodVoteController.java
@@ -11,16 +11,15 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.iphayao.repository.Holiday_Repo;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
-@CrossOrigin("*")
-@RequestMapping("/voteFood")
+@CrossOrigin("/voteFood")
 public class DashboardFoodVoteController {
 	
 	@Autowired
@@ -28,7 +27,7 @@ public class DashboardFoodVoteController {
 	private NamedParameterJdbcTemplate jdbcTemplate = null;
 	private StringBuilder stb = null;
 
-	@GetMapping("/dashboardFoodVote")
+	@RequestMapping("/dashboardFoodVote")
 	public ArrayList<Map<String, Object>> findEmp(String empCode) {
 		System.out.println("Stay in ChartController");
 		ArrayList<Map<String, Object>> result = null;
