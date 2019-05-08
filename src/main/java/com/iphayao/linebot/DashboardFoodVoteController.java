962f0a324c9bb0c5febd6cf5db10f01c2c928d65
CHALARM CHAIWONG <5831305013@lamduan.mfu.ac.th>
2019-05-08 09:09:33 +0700

    no message

--- src/main/java/com/iphayao/linebot/DashboardFoodVoteController.java
+++ src/main/java/com/iphayao/linebot/DashboardFoodVoteController.java
@@ -18,7 +18,7 @@ import com.iphayao.repository.Holiday_Repo;
 
 import lombok.extern.slf4j.Slf4j;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 @Slf4j
 @Controller
 public class DashboardFoodVoteController {
