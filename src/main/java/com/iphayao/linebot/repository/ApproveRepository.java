5725c24e81373458e59b90a75adf9bb064f24033
May Kanchana <kanchana.khadmai@gmail.com>
2019-07-26 11:19:13 +0700

    no message

--- src/main/java/com/iphayao/linebot/repository/ApproveRepository.java
+++ src/main/java/com/iphayao/linebot/repository/ApproveRepository.java
@@ -51,6 +51,7 @@ public class ApproveRepository {
 
 	public String approve(Customer data) {
 		ArrayList<Map<String, Object>> result = null;
+		ArrayList<Map<String, Object>> account_id = null;
 		String random;
 		try {
 			jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
@@ -70,25 +71,32 @@ public class ApproveRepository {
 				jdbcTemplate.update(stb.toString(), parameters);
 				stb.setLength(0);
 
-				stb2 = new StringBuilder();
-				stb2.append(" Select customer_user_line_id From customer");
-				stb2.append(" WHERE customer_user_id = :customer_user_id ");
-	
-				MapSqlParameterSource parameters2 = new MapSqlParameterSource();
-				parameters2.addValue("customer_user_id", data.getCustomer_code());
-				result = (ArrayList<Map<String, Object>>) jdbcTemplate.queryForList(stb2.toString(), parameters2);
+				stb.append(" Select account_id From account");
+				stb.append(" WHERE customer_user_id = :customer_user_id ");
+
+				parameters.addValue("customer_user_id", data.getCustomer_code());
+				account_id = (ArrayList<Map<String, Object>>) jdbcTemplate.queryForList(stb.toString(), parameters);
 
 				stb.setLength(0);
 				stb.append(
 						" INSERT INTO payment(account_id, status_id, payment_amount_paid, payment_period, payment_principle, ");
 				stb.append(
-						" payment_installment, payment_outstanding_balance, payment_pay_date, payment_pay_date_next, created_by, created_date, created_program, updated_by, updated_date, updated_program, payment_desc) ");
+						" payment_installment, payment_outstanding_balance, payment_pay_date_next, created_by, created_date, created_program, updated_by, updated_date, updated_program) ");
 				stb.append(
-						" VALUES (:account_no, :customer_user_id, 'SS-Pico-Finance', NOW(), 'SS-Pico-Finance','SS-Pico-Finance', NOW(), 'SS-Pico-Finance') ");
+						" VALUES (:account_id, :status_id, :payment_amount_paid, :payment_period, :payment_principle, :payment_installment, :payment_outstanding_balance ");
+				stb.append(
+						" ,to_timestamp(extract(year from now())::TEXT || '-' || lpad(extract(month from now() + INTERVAL '1 month')::text, 2, '0') || '-20', 'yyyy-mm-dd') ");
+				stb.append(
+						" ,'SS-Pico-Finance', NOW(), 'SS-Pico-Finance','SS-Pico-Finance', NOW(), 'SS-Pico-Finance')");
 
-				parameters.addValue("account_no", random);
-				parameters.addValue("customer_user_id", data.getCustomer_code());
-				parameters.addValue("account_approve", data.getApprove_status());
+				parameters.addValue("account_id", account_id.get(0).get("account_id"));
+				parameters.addValue("status_id", 1);
+				parameters.addValue("payment_period", "1");
+				parameters.addValue("payment_amount_paid", 11050);
+				parameters.addValue("payment_principle", 10000);
+				parameters.addValue("payment_installment", 1050);
+				parameters.addValue("payment_outstanding_balance", 90000);
+
 				jdbcTemplate.update(stb.toString(), parameters);
 			}
 
