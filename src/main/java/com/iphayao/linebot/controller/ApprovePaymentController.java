3f3cceb604e6f67ba6aa6253720cc6d94a223f47
youthasilpa_s <youthasilpa.sirivedin@gmail.com>
2019-08-02 16:58:13 +0700

    line03

--- src/main/java/com/iphayao/linebot/controller/ApprovePaymentController.java
+++ src/main/java/com/iphayao/linebot/controller/ApprovePaymentController.java
@@ -1,61 +1,41 @@
-// package com.iphayao.linebot.controller;
+ package com.iphayao.linebot.controller;
 
-// import java.util.Arrays;
-// import java.util.List;
-// import java.util.concurrent.ExecutionException;
+import java.util.ArrayList;
+import java.util.Map;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.context.annotation.ComponentScan;
-// import org.springframework.dao.DataIntegrityViolationException;
-// import org.springframework.web.bind.annotation.CrossOrigin;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-// import com.iphayao.linebot.model.Customer;
-// import com.iphayao.linebot.repository.ApprovePaymentRepository;
-// import com.linecorp.bot.client.LineMessagingClient;
-// import com.linecorp.bot.model.PushMessage;
+import com.iphayao.linebot.repository.ApprovePaymentRepository;
+import com.iphayao.linebot.repository.ApprovePaymentRepository.ModelUpdate;
 
-// import com.linecorp.bot.model.message.Message;
-// import com.linecorp.bot.model.message.TextMessage;
-// import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+ @CrossOrigin
+ @RestController
+ @RequestMapping(path = "/approvepaymant")
+ public class ApprovePaymentController {
 
-// // import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+     @Autowired
+     private ApprovePaymentRepository approvePayRepo;
 
-// import lombok.NonNull;
+     @GetMapping(path = "/search")
+     public ArrayList<Map<String, Object>> searchPaymant(@RequestParam(value = "keyword") String keyword) throws Exception {
+    	 return approvePayRepo.searchPaymant(keyword);
+     }
+     
+     @GetMapping(path = "/searchUpdate")
+     public Map<String, Object> searchPaymantUpdate(@RequestParam(value = "paymentId") Integer paymentId) throws Exception {
+    	 return approvePayRepo.searchPaymantUpdate(paymentId);
+     }
+     
+     @PutMapping(path = "/update")
+     public Map<String, Object> update(@RequestBody ModelUpdate model) throws Exception {
+    	 return approvePayRepo.Update(model);
+     }
 
-// @ComponentScan
-// @LineMessageHandler
-// @CrossOrigin
-// @RestController
-// @RequestMapping(path = "/approve")
-
-// public class ApprovePaymentController {
-
-//     @Autowired
-//     private LineBotController LineBotController;
-//     @Autowired
-//     private ApprovePaymentRepository approvePayRepo;
-
-//     private LineMessagingClient lineMessagingClient;
-
-//     @PostMapping(path = "/submitpayment")
-//     public void updateApprove(@RequestBody Customer data) throws Exception {
-//        String userId; 
-//        String approveStatus; 
-//         try {
-//             if(data.getApprove_status() == true){
-//                 approveStatus = "หลักฐานการชำระเงินของคุณได้ผ่านการยืนยันเรียบร้อยแล้ว";
-//             }else{
-//                 approveStatus = "หลักฐานการชำระเงินของคุณไม่ผ่านการยืนยัน สามารถสอบถามเพิ่มเติมได้ที่ 02-222-2222";
-//             }
-//             userId = approvePayRepo.approve(data);
-//             LineBotController.push(userId, Arrays.asList(new TextMessage(approveStatus)));
-//         } catch (DataIntegrityViolationException e) {
-//             throw e;
-//         }
-//     }
-
-// }
+ }
